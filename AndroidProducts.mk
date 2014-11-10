48bb49472f10077c55c41058aa1869e24e9db1b8
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2014-11-10 21:46:41 -0100

    Du-ify for Lollipop
    Change-Id: I0ab865ced381c6844abde638b10550219fad660a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,5 +17,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_hammerhead.mk \
     $(LOCAL_DIR)/full_hammerhead.mk \
-    $(LOCAL_DIR)/car_hammerhead.mk
+    $(LOCAL_DIR)/car_hammerhead.mk \
+    $(LOCAL_DIR)/du_hammerhead.mk
 
