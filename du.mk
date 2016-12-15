e3f242a656b9e34eba7777a87f4d849991c5d149
Alex Cruz <mazdarider23@gmail.com>
2016-12-15 17:47:26 +0100

    Remove nfc_enhanced.mk
    - Remove overlay for mobile plan, is off by default in settings
    
    Change-Id: I8b321adfd931f297355df756895e338721af8989

--- du.mk
+++ du.mk
@@ -22,7 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := du_hammerhead
 PRODUCT_DEVICE := hammerhead
