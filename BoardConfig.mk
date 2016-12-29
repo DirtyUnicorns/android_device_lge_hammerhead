6d7b1de450f67e3697fbb617d9bba74839a1a78e
Steven Moreland <smoreland@google.com>
2016-12-28 16:27:03 -0800

    Dumpstate: convert to hidl implementation.
    Bug: 31982882
    Test: wip
    Change-Id: I27d661e5339addb0c65e6fc86969698eb529e25f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,8 +110,6 @@ TARGET_RECOVERY_FSTAB = device/lge/hammerhead/fstab.hammerhead
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/hammerhead
 
-BOARD_HAL_STATIC_LIBRARIES := libdumpstate.hammerhead
-
 BOARD_SEPOLICY_DIRS += device/lge/hammerhead/sepolicy
 
 HAVE_ADRENO_SOURCE:= false
