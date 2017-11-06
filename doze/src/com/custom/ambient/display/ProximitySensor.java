7b98b93c6f1f9171dddc9999792d272915c13f71
Alex Cruz <du.alexcruz@gmail.com>
2017-11-06 12:32:49 -0500

    Change package name for uniformity
    With Android O we want to stick to just one package name for device specific doze.

--- doze/src/com/custom/ambient/display/ProximitySensor.java
+++ doze/src/com/custom/ambient/display/ProximitySensor.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package com.custom.ambient.display;
 
 import android.content.Context;
 import android.hardware.Sensor;
